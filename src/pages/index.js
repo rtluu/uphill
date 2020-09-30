14705b5415c1f8dc386327b5e98a0844ea3764c8
Ryan Luu <ryan@vumble.com>
2020-09-29 22:07:29 -0400

    Started Uphill repo with Gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Uphill Podcast</h1>
+    <p>A podcast about the uphill pursuit to becoming your best self.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
+
+export default IndexPage
