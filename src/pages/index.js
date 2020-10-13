0dd84597f385c4d00b5eb611e8f8c9a94391e127
Ryan Luu <ryan@vumble.com>
2020-10-12 21:53:19 -0400

    First Episodes

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,12 +20,24 @@ const IndexPage = () => (
       <p className="hosted">Hosted by Ryan Luu & Landon Galloway</p>
     </div>
 
-    <div className="description">
+    <div className="description-box">
       <div className="description-inner">
         <div className="description-image">
           <LogoImage />
         </div>
         <p className="description-text">This is a podcast designed to uplift listeners on the uphill path of life that we all must endure. We are seeking to help individuals reach their potential, and to promote progress. Throughout this journey we will touch on past experiences, personal struggles, and our approaches to life & what that means to us. Physically, we look to challenge ourselves with rigorous exercise routines; mentally, we are looking to grow and expand our base of knowledge; spiritually, we are looking for enlightenment and the vitality surrounding meaningful relationships.</p>
+      </div>
+    </div>
+
+    <div className="episodes-box">
+      <div className="episodes-inner">
+        <div className="episode-card">
+          <iframe frameBorder="0" height="180" width="100%" src="https://player.podcard.fm?color=192BC2&episode=We're%20Going%20on%20Summer%20Vacation!&feed=https://anchor.fm/s/34c65550/podcast/rss"></iframe>
+        </div>
+
+        <div className="episode-card">
+          <iframe frameBorder="0" height="180" width="100%" src="https://player.podcard.fm?color=ED6A5A&episode=2&feed=https://anchor.fm/s/34c65550/podcast/rss"></iframe>
+        </div>
       </div>
     </div>
   </Layout>
